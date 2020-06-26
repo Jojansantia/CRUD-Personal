91a69f676206250a305fe64340abe9e76e8cf843
Jojansantia <jojansantia0317@gmail.com>
2020-06-26 15:29:03 -0500

    README

--- src/components/Personal.js
+++ src/components/Personal.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import Persona from './Persona';
 
+import { MetroSpinner } from 'react-spinners-kit';
 import { useSelector, useDispatch } from 'react-redux';
 import { obtenerPersonalAction } from '../actions/personaActions';
 
@@ -20,34 +21,35 @@ const Personal = () => {
 
     return ( 
         <>
-            <h1 className="text-center text-gray-700 text-2xl font-bold text-black ">
+            <h1 className=" mb-2 text-center text-gray-700 text-2xl font-bold text-black ">
                 Listado
             </h1>
             
-            { error ? <p className="font-weight-bold alert alert-danger text-center mt-4">Hubo un error</p> : null }
-        
-            { cargando ? <p className="text-center">Cargando....</p> : null }
-            
-            <table className="text-center w-4/5 m-auto">
-               <thead >
-                    <tr>
-                        <th className="border bg-gray-200 text-gray-700 px-4 py-2">Nombre</th>
-                        <th className="border bg-gray-200 text-gray-700 px-4 py-2">Edad</th>
-                        <th className="border bg-gray-200 text-gray-700 px-4 py-2">Acciones</th>
-                    </tr>
-               </thead>
-               <tbody>
-                   { personal.length !== 0 &&  (
-                       personal.map(persona => (
-                           <Persona
-                                key={persona.id}
-                                persona={persona}
-                           />
-                       ))
-                   ) }
-               </tbody>
-           </table>
+            { error ? <p className="bg-red-400 font-bold text-center  w-4/5 m-auto p-3 my-2">Hubo un error</p> : null }
 
+            { cargando ? 
+                <div className="flex justify-center my-1"> <MetroSpinner  color="#000" /> </div> 
+            :
+                <table className="text-center w-4/5 m-auto">
+                    <thead >
+                            <tr>
+                                <th className="border bg-gray-200 text-gray-700 px-4 py-2">Nombre</th>
+                                <th className="border bg-gray-200 text-gray-700 px-4 py-2">Edad</th>
+                                <th className="border bg-gray-200 text-gray-700 px-4 py-2">Acciones</th>
+                            </tr>
+                    </thead>
+                    <tbody>
+                        { personal.length !== 0 &&  (
+                            personal.map(persona => (
+                                <Persona
+                                        key={persona.id}
+                                        persona={persona}
+                                />
+                            ))
+                        ) }
+                    </tbody>
+                </table>
+            }
         </>
     );
 }
